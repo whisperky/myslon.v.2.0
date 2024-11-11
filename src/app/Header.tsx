2e9af3cc1bf3facf1b43990929e4fbde97e0c4ae
whisperky <josephcollins1210@gmail.com>
2024-11-11 06:14:17 -0500

    create: header draft created

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,3 +1,76 @@
+import { Button, Flex, Image, Link, Text } from "@chakra-ui/react";
+
+const BgGradient = () => {
+  return (
+    <svg
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "500%",
+        top: 0,
+        left: 0,
+        zIndex: 0,
+      }}
+      viewBox="0 80 100 100"
+      preserveAspectRatio="none"
+    >
+      <defs>
+        <radialGradient id="headerGradient" cx="50%" cy="100%" r="100%">
+          <stop offset="0%" stopColor="#EEA22D55" />
+          <stop offset="80%" stopColor="#00000000" />
+        </radialGradient>
+      </defs>
+      <rect x="0" y="0" width="100" height="100" fill="url(#headerGradient)" />
+    </svg>
+  );
+};
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Flex
+      position="fixed"
+      w="70%"
+      top="0"
+      left="0"
+      right="0"
+      zIndex={100}
+      justifyContent="space-between"
+      alignItems="center"
+      mx="auto"
+      py="4"
+      borderBottom="2px solid transparent"
+      sx={{
+        borderImage: "linear-gradient(to right, #000000, #EEA22D, #000000) 1",
+      }}
+    >
+      <Image src="img/logo.png" h="10" zIndex={1} />
+      <Flex gap={6} alignItems="center" color="white" zIndex={1}>
+        <Text color="white" fontSize="16px">
+          CHAPTERS
+        </Text>
+        <Flex gap={4}>
+          <Link href="/">
+            <Button variant="outline" color="white">
+              T
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button variant="outline" color="white">
+              R
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button variant="outline" color="white">
+              D
+            </Button>
+          </Link>
+        </Flex>
+        <Button variant="outline" color="white">
+          Log in / Sign up
+        </Button>
+      </Flex>
+
+      <BgGradient />
+    </Flex>
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(169.40% 89.55% at 94.76% 6.29%, rgba(0, 0, 0, 0.40) 0%, rgba(255, 255, 255, 0.00) 100%)',
+      },
     },
   },
   plugins: [],
